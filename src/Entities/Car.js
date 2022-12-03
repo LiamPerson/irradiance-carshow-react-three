9449e5e7ed54a5b7a9fb7b25c6c18d9bb606dd51
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-12-03 22:45:19 +1000

    added car

---
+++ src/Entities/Car.js
@@ -0,0 +1,26 @@
+import { useGLTF } from "@react-three/drei";
+import React, { useEffect } from "react";
+import { Mesh } from "three";
+
+const Car = () => {
+
+    const gltf = useGLTF("/models/car/scene.gltf");
+
+    useEffect(() => {
+        gltf.scene.scale.set(0.005, 0.005, 0.005);
+        gltf.scene.position.set(0, -0.035, 0);
+        gltf.scene.traverse((object) => {
+            if(object instanceof Mesh) {
+                object.castShadow = true;
+                object.receiveShadow = true;
+                object.material.envMapIntensity = 20;
+            }
+        })
+    }, [gltf]);
+
+    return (
+        <primitive object={gltf.scene} />
+    )
+}
+
+export default Car;
