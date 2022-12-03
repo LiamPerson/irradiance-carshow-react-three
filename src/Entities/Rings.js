54b1704f1ea5c77303645d45447619bb9e9e6ab6
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-12-03 22:45:10 +1000

    added luminous rings

---
+++ src/Entities/Rings.js
@@ -0,0 +1,48 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react"
+import { Color } from "three";
+
+const Rings = () => {
+    const itemsRef = useRef([]);
+
+    useFrame(state => {
+        // Go through each ring and set the position
+        for(let i = 0; i < itemsRef.current.length; i++) {
+            let mesh = itemsRef.current[i];
+            let z = (i - 7) * 3.5;
+            mesh.position.set(0, 0, -z); // Set the position of each ring
+
+            let dist = Math.abs(z);
+            mesh.scale.set(1 - dist * 0.04, 1 - dist * 0.04, 1 - dist * 0.04);
+
+            let colorScale = 1;
+            if (dist > 2) {
+                colorScale = 1 - (Math.min(dist, 12) - 2) / 10; // Scale out colours with distance
+            }
+            colorScale *= .5;
+
+            // Colour odd rings red and even rings blue
+            if(i % 2 === 1) {
+                mesh.material.emissive = new Color(6, .15, .7).multiplyScalar(colorScale);
+            } else {
+                mesh.material.emissive = new Color(.1, .7, 3).multiplyScalar(colorScale);
+            }
+        }
+    })
+
+    return (<>
+        {Array.from({ length: 14 }).map((v, i) => (
+            <mesh
+                castShadow
+                receiveShadow
+                position={[0,0,0]}
+                ref={el => itemsRef.current[i] = el}
+            >
+                <torusGeometry args={[3.35, 0.05, 16, 100]} />
+                <meshStandardMaterial emissive={[0.5, 0.5, 0.5]} color={[0,0,0]} />
+            </mesh>
+        ))}
+    </>)
+}
+
+export default Rings;
